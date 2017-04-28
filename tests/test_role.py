330b52efbb8c0c54e879b425d13776394dd6a243
Christian Berendt <berendt@betacloud-solutions.de>
2017-04-28 10:18:15 +0200

    Sync with template

--- tests/test_role.py
+++
@@ -1,7 +0,0 @@
-# This file is subject to the terms and conditions defined in file 'LICENSE',
-# which is part of this repository.
-
-
-def test_packages_are_installed(Package):
-    package = Package("git")
-    assert package.is_installed
